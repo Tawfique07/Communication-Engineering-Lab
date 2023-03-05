6da169b99f7a1cc62e2fe6152e8be103b92b3953
tawfique007 <mdtoufiq2342@gmail.com>
2023-03-05 15:24:14 +0600

    demodulation added

--- B8ZS.m
+++ B8ZS.m
@@ -2,7 +2,7 @@ clc; #... Clear command line
 clear all; #... Clear variables
 close all; #... Clear figures
 
-bits = [ 1 0 0 0 0 0 0 0 0 0 1];
+bits = [ 1 0 0 0 0 0 0 0 0 1];
 
 #... Modulation
 
@@ -16,7 +16,7 @@ endTime = length(bits)/bitrate;
 time = 0:samplingTime:endTime;
 
 index = 1;
-sign = -1;
+sign = 1;
 
 for i=1:length(time)
 	if bits(index) == 0
@@ -57,32 +57,29 @@ disp(demodulation);
 
 #...scrambling
 
+cnt = 0
 for i=1:length(bits)
-	cnt = 0
-	for j=i:i+8
-		if j>length(bits)
-			break;
-		endif
-		if bits(j)==1
-			break;
-		else
-			cnt = cnt + 1;
-		endif
-	endfor
+	if bits(i)==1
+		cnt = 0;
+		continue;
+	endif
 	
-	if cnt==8
-		bits(i+2) = -1;
-		bits(i+3) = 1;
-		bits(i+5) = -1;
-		bits(i+6) = 1;
+	if bits(i) == 0
+		cnt = cnt +1;
+	endif
+	
+	if cnt == 8
+		cnt = 0;
+		bits(i) = 1;
+		bits(i-1) = -1;
+		bits(i-3) = 1;
+		bits(i-4) = -1;
 	endif
 	
 end
 
-disp(bits);
-
 index = 1;
-sign = -1;
+sign = 1;
 
 for i=1:length(time)
 	if bits(index) == 0
@@ -103,3 +100,37 @@ subplot(2,1,2);
 plot(time, modulation, "LineWidth", 2);
 axis([0 endTime -voltage-5 voltage+5]);
 grid on;
+
+#... demodulation
+
+index = 1;
+prev = -voltage;
+
+for i= 1:length(modulation)
+	if prev != modulation(i)
+		demodulation(index) = 1;
+	else
+		demodulation(index) = -1;
+	endif
+	
+	if modulation(i) == 0
+		demodulation(index) = 0;
+	endif
+	
+	if time(i)*bitrate >= index
+		if modulation(i)!=0
+			prev = modulation(i);
+		endif
+		index = index+1;
+	endif
+end
+
+for i=1:length(demodulation)
+	if demodulation(i) == -1
+		demodulation(i) = 0;
+		demodulation(i+1) = 0;
+	endif
+end
+
+disp(demodulation);
+
